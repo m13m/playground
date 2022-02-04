8835040f70ee02c6ead13bfc889b0d7f34b637a4
Mohd Maqbool Alam <maqbool@maqbool.net>
2022-02-04 10:23:40 +0530

    sample ios app dev setup

---
+++ foobar/foobar/foobarApp.swift
@@ -0,0 +1,17 @@
+//
+//  foobarApp.swift
+//  foobar
+//
+//  Created by Mohd Maqbool Alam on 30/01/22.
+//
+
+import SwiftUI
+
+@main
+struct foobarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ios/foobar/foobar/foobarApp.swift
@@ -0,0 +1,17 @@
+//
+//  foobarApp.swift
+//  foobar
+//
+//  Created by Mohd Maqbool Alam on 04/02/22.
+//
+
+import SwiftUI
+
+@main
+struct foobarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
